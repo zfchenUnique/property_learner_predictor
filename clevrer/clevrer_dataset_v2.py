420cf8c5ea9d18c13475260606721b84062c4f5d
Zhenfang Chen <zfchen@cs.hku.hk>
2021-05-25 06:38:28 +0800

    [YZ] add augmentation to the input data

--- clevrer/clevrer_dataset_v2.py
+++ clevrer/clevrer_dataset_v2.py
@@ -280,7 +280,9 @@ class clevrerDataset(Dataset):
         obj_ftr = torch.from_numpy(obj_ftr)
         edge = torch.from_numpy(edge)
         # batch size 1  & time slide 1
-        x = obj_ftr[:, :n_his+1].view(1, obj_ftr.shape[0], 1,  -1)
+        x = obj_ftr[:, :n_his+1]
+        x[:, :, 4:] = x[:, :, 4:] + torch.randn(x[:, :, 4:].size()) * 0.005
+        x = x.view(1, obj_ftr.shape[0], 1, -1)
         label = obj_ftr[:, n_his+1:].view(1, obj_ftr.shape[0], n_roll, -1)
         edge = edge.view(1, -1)
         '''
